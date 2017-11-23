4ab5f323be0349d002f7c3b0417c76a3c4f0bdea
Bruno Lima <bgcl@ic.ufal.br>
2017-11-23 15:05:04 -0200

    Checked MPU status is working

--- button.c
+++ button.c
@@ -8,17 +8,11 @@
 #include "espressif/esp_common.h"
 #include "esp/uart.h"
 #include "FreeRTOS.h"
-#include <i2c/i2c.h>
+#include "i2c.h"
 #include "task.h"
 #include "queue.h"
 #include "esp8266.h"
 #include "bma2x2.h"
-
-/* I2C definitions  */
-#define I2C_BUS 0
-#define SCL_PIN 5
-#define SDA_PIN 4
-#define ADDR 0x68
 
 /* pin config */
 const int gpio = 0;   /* gpio 0 usually has "PROGRAM" button attached */
@@ -82,15 +76,43 @@ void gpio_intr_handler(uint8_t gpio_num)
     xQueueSendToBackFromISR(tsqueue, &now, NULL);
 }
 
-uint8_t read_reg_mpu(uint8_t reg){
-	i2c_start(I2C_BUS);
-	bool acked = i2c_write(I2C_BUS, ADDR);
-	if( acked ) {
-		printf("Byte was acked.\n");
-	} else {
-		printf("Byte was not acked.\n");
-	}
-	i2c_stop(I2C_BUS);		
+
+/* I2C definitions  */
+// 5 e 4
+// 0 e 2
+#define I2C_BUS 0
+#define ADDR 0x68
+#define WHO_I_AM     0x75 // decimal 117
+#define AM_I_SLEEP   0x6B // decimal 107
+#define SCL_PIN (5)
+#define SDA_PIN (4)
+
+uint8_t init_MPU(){
+
+    int regs[] = {107,0x1B,0x1C};
+    for (int i=0;i<3;i++) {
+        uint8_t data[] = {regs[i], 0};
+        i2c_slave_write(ADDR, data, sizeof(data));
+
+    }
+}
+
+void check_MPU() {
+    uint8_t reg_data = -1;
+    i2c_slave_read(ADDR, WHO_I_AM, &reg_data, 1);
+    if ( reg_data == 0x68 ) {
+        printf("MPU6050 is allright! Everything is fine!\n");
+    } else {
+        printf("MPU6050 is allright! Everything is fine!\n");
+    }
+
+    i2c_slave_read(ADDR, AM_I_SLEEP, &reg_data, 1);
+    if ( reg_data == 0x64 ) {
+        printf("MPU6050 is in SLEEP mode!\n");
+    } else {
+        printf("MPU6050 is ACTIVE.\n");
+    }
+
 }
 
 void user_init(void)
@@ -99,17 +121,44 @@ void user_init(void)
     uart_set_baud(0, 115200);
     gpio_enable(gpio, GPIO_INPUT);
 
-    int conn = i2c_init(I2C_BUS, SCL_PIN, SDA_PIN, I2C_FREQ_100K);
-    if( !conn ) {
-	printf("Starting I2C connection.\n");
+    uint8_t slave_addr = 0x68;  // MPU address
+    uint8_t reg_addr = 0x107;   //
+    uint8_t data[] = {reg_addr, 0};
+
+    i2c_init(SCL_PIN, SDA_PIN);
+    bool success = i2c_slave_write(slave_addr, data, sizeof(data));
+    if (success)
+    {
+        printf("YAY!\n");
     } else {
-	printf("Connection not started. Check the pins.\n");
+        printf("AFF :(\n");
     }
 
-    read_reg_mpu(0);
-    
+    init_MPU();
+    check_MPU();
 
  
+//	// Requisitando dados
+//	i2c_start(I2C_BUS);
+//	i2c_write(I2C_BUS, ADDR);
+//	i2c_write(I2C_BUS, 0x3B);
+//	// Lendo dados
+//	i2c_start(I2C_BUS);
+//	i2c_write(I2C_BUS, ADDR);
+//	uint8_t data = i2c_read(I2C_BUS, true);
+//	printf("Data read: %x\n", data);
+//	i2c_stop(I2C_BUS);
+
+//	// WHO I AM
+//	i2c_start(I2C_BUS);
+//	i2c_write(I2C_BUS, ADDR);
+//	i2c_write(I2C_BUS, WHO_I_AM);
+//	i2c_start(I2C_BUS);
+//	uint8_t who = i2c_read(I2C_BUS, true);
+//	i2c_stop(I2C_BUS);
+//	printf("Data WHO: %x\n", who);
+	
+
 
     tsqueue = xQueueCreate(2, sizeof(uint32_t));
     xTaskCreate(buttonIntTask, "buttonIntTask", 256, &tsqueue, 2, NULL);
